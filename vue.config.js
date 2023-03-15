c2053411e13af4b5bf71d9e219b6a43055d78764
Kirill Katashinsky <kir20-01@ya.ru>
2023-03-16 01:00:27 +0300

    Some small changes in configs

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  productionSourceMap: false,
+  lintOnSave: process.env.NODE_ENV !== 'production'
+});

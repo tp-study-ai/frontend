3060958a02e80354e077f26d883812c0fccf2a5c
Kirill Katashinsky <kir20-01@ya.ru>
2023-04-08 15:53:58 +0300

    Changed tasks page

--- src/components/tasks-page/index.vue
+++ src/components/tasks-page/index.vue
@@ -1,33 +1,38 @@
 <template>
-<div>
-  <div v-if="loading">
-    <v-progress-circular indeterminate color="primary" />
-  </div>
+<v-container class="px-0">
+  <v-data-table
+    :headers="tableHeaders"
+    :items="tasks"
+    :items-per-page="15"
+    :loading="loading"
+    hide-default-footer
+    @update:sort-desc="updateSorting"
+  >
+    <template #item="{ item, isMobile }">
+      <v-card v-if="isMobile" class="mb-4">
+        <v-card-title>{{ item.name }}</v-card-title>
+        <v-card-text>
+          <div>Сложность: {{ item.difficulty }}</div>
+          <div>Рейтинг: {{ item.cf_rating - 6 }}</div>
+        </v-card-text>
 
-  <div v-else>
-    <v-simple-table>
-      <thead>
-        <tr>
-          <th>Название</th>
-          <th>Сложность</th>
-          <th>Рейтинг</th>
-        </tr>
-      </thead>
+        <v-card-actions>
+          <v-btn :to="getTaskPath(item)" text color="primary">Перейти</v-btn>
+        </v-card-actions>
+      </v-card>
 
-      <tbody>
-        <tr v-for="task in tasks" :key="task.id">
-          <td>
-            <router-link :to="getTaskPath(task)">{{ task.name }}</router-link>
-          </td>
-          <td>{{ task.difficulty }}</td>
-          <td>{{ task.cf_rating - 6 }}</td>
-        </tr>
-      </tbody>
-    </v-simple-table>
+      <tr v-else>
+        <td>
+          <router-link :to="getTaskPath(item)">{{ item.name }}</router-link>
+        </td>
+        <td>{{ item.difficulty }}</td>
+        <td>{{ item.cf_rating - 6 }}</td>
+      </tr>
+    </template>
+  </v-data-table>
 
-    <v-pagination v-model="page" :total-visible="totalLength" :length="522" />
-  </div>
-</div>
+  <v-pagination v-model="page" :length="522" />
+</v-container>
 </template>
 
 <script>
@@ -37,18 +42,26 @@ export default {
     return {
       loading: true,
       page: 1,
+      sort: 'rating_up',
       tasks: []
     };
   },
   computed: {
-    totalLength() {
-      if (this.$vuetify.breakpoint.xsAndDown) {
-        return 7;
-      }
-      if (this.$vuetify.breakpoint.smAndDown) {
-        return 15;
-      }
-      return 20;
+    tableHeaders() {
+      return [
+        {
+          text: 'Название',
+          sortable: false
+        },
+        {
+          text: 'Сложность',
+          sortable: false
+        },
+        {
+          text: 'Рейтинг',
+          sortable: true
+        }
+      ];
     }
   },
   watch: {
@@ -60,16 +73,27 @@ export default {
     if (this.$route.query.page) {
       this.page = parseInt(this.$route.query.page);
     }
+    if (this.$route.query.sort) {
+      this.sort = this.$route.query.sort;
+    }
     this.getTasks();
   },
   methods: {
     getTasks() {
+      const query = {};
       if (this.$route.query.page !== this.page.toString()) {
-        this.$router.push({ path: 'tasks', query: { page: this.page }});
+        query.page = this.page;
+      }
+      if (this.$route.query.sort !== this.sort) {
+        query.sort = this.sort;
+      }
+
+      if (Object.keys(query).length > 0) {
+        this.$router.push({ path: 'tasks', query });
       }
 
       this.loading = true;
-      const params = { page: this.page - 1 };
+      const params = { page: this.page - 1, sort: this.sort };
 
       this.$http
         .get('/tasks_list', { params })
@@ -82,6 +106,10 @@ export default {
     },
     getTaskPath(task) {
       return `/task/${task.id}`;
+    },
+    updateSorting(sorting) {
+      this.sort = sorting ? 'rating_down' : 'sorting_up';
+      this.getTasks();
     }
   }
 }

734fa2b126bb8fbf8bcf22812e4b15b29ef39f5e
Kirill Katashinsky <kir20-01@ya.ru>
2023-04-11 23:32:53 +0300

    Changed indents

--- src/components/task-page/components/examples-tab.vue
+++ src/components/task-page/components/examples-tab.vue
@@ -1,12 +1,12 @@
 <template>
 <div>
   <div v-for="(example, id) in examples" :key="id">
-    <v-card class="mb-2 mx-4 mt-4">
+    <v-card class="mb-2 mx-4 mt-4 pt-2">
       <div class="ml-4 font-bold">Входные данные</div>
-      <v-card-text v-html="example.input" />
+      <v-card-text v-html="example.input" class="pt-2" />
 
       <div class="ml-4 font-bold">Выходные данные</div>
-      <v-card-text v-html="example.output" />
+      <v-card-text v-html="example.output" class="py-2" />
     </v-card>
   </div>
 </div>

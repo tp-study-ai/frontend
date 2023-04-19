ebcf976ea2937c00172296cf074b1c70e4f3ec1a
Kirill Katashinsky <kir20-01@ya.ru>
2023-04-19 18:15:16 +0300

    Changed height

--- src/components/task-page/index.vue
+++ src/components/task-page/index.vue
@@ -125,7 +125,7 @@
             :copy_code="false"
             count_lines
             width="auto"
-            height="calc(80vh - 5px)"
+            height="calc(83vh - 5px)"
             border_radius="0px"
             theme="light"
           />
@@ -519,13 +519,13 @@ export default {
 
 <style>
 .left-card {
-  min-height: 80vh;
-  max-height: 80vh;
+  min-height: 83vh;
+  max-height: 83vh;
 }
 
 .right-card {
-  min-height: calc(80vh + 48px);
-  max-height: calc(80vh + 48px);
+  min-height: calc(83vh + 48px);
+  max-height: calc(83vh + 48px);
 }
 
 /* Используется для стилизации заголовков внутри описания задачи */

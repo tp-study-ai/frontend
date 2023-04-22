2cec1b453b35ba19d22408d099303f7175fb8e29
Kirill Katashinsky <kir20-01@ya.ru>
2023-04-22 12:50:32 +0300

    Changed task page

--- src/components/task-page/components/examples-tab.vue
+++ src/components/task-page/components/examples-tab.vue
@@ -1,51 +1,51 @@
 <template>
 <div>
-  <v-card
-    :class="{ 'mt-4 pt-2': $vuetify.breakpoint.mdAndUp }"
-    class="mx-4 pb-2 d-flex justify-space-between"
-    flat
-  >
-    <div class="text-subtitle-1">Ограничение по памяти: {{ memoryLimit }} Мб</div>
-    <div class="text-subtitle-1">Ограничение по времени: {{ limits.time_limit }} с</div>
+  <v-card :class="{ 'mt-4': $vuetify.breakpoint.mdAndUp }" flat>
+    <v-card-text class="d-flex justify-space-between pb-0">
+      <div class="text-subtitle-1">Ограничение по памяти: {{ memoryLimit }} Мб</div>
+      <div class="text-subtitle-1">Ограничение по времени: {{ limits.time_limit }} с</div>
+    </v-card-text>
   </v-card>
 
-  <div class="mx-4 py-2 d-flex justify-space-between" outlined>
-    <div class="text-subtitle-1">Ввод: стандартный ввод</div>
-    <div class="text-subtitle-1">Вывод: стандартный вывод</div>
-  </div>
+  <v-card class="d-flex justify-space-between" flat>
+    <v-card-text class="d-flex justify-space-between pb-2">
+      <div class="text-subtitle-1">Ввод: стандартный ввод</div>
+      <div class="text-subtitle-1">Вывод: стандартный вывод</div>
+    </v-card-text>
+  </v-card>
 
   <div v-if="examples.length > 0">
-    <v-card v-for="(example, id) in examples" :key="id" class="mb-2 mx-4 mt-4 py-2 px-4" outlined>
-      <div class="text-subtitle-1 font-weight-bold">
-        Входные данные
-        <v-btn
-          v-if="$vuetify.breakpoint.mdAndUp"
-          class="ml-1"
-          icon
-          @click="copyText(example.input)"
-        >
-          <v-icon>mdi-content-copy</v-icon>
-        </v-btn>
-      </div>
-      <v-card-text class="pl-0 pt-0">
+    <v-card v-for="(example, id) in examples" :key="id" class="mx-4 mt-4" outlined>
+      <v-card-text>
+        <div class="text-subtitle-1 font-weight-bold px-0">
+          Входные данные
+          <v-btn
+            v-if="$vuetify.breakpoint.mdAndUp"
+            class="ml-1"
+            icon
+            @click="copyText(example.input)"
+          >
+            <v-icon>mdi-content-copy</v-icon>
+          </v-btn>
+        </div>
         <div v-for="(line, index) in getInputLines(example)" :key="`input_${index}_line`">
           {{ line }}
           <br />
         </div>
       </v-card-text>
 
-      <div class="text-subtitle-1 font-weight-bold">
-        Выходные данные
-        <v-btn
-          v-if="$vuetify.breakpoint.mdAndUp"
-          class="ml-1"
-          icon
-          @click="copyText(example.output)"
-        >
-          <v-icon>mdi-content-copy</v-icon>
-        </v-btn>
-      </div>
-      <v-card-text class="pl-0 pt-0 pb-0">
+      <v-card-text>
+        <div class="text-subtitle-1 font-weight-bold">
+          Выходные данные
+          <v-btn
+            v-if="$vuetify.breakpoint.mdAndUp"
+            class="ml-1"
+            icon
+            @click="copyText(example.output)"
+          >
+            <v-icon>mdi-content-copy</v-icon>
+          </v-btn>
+        </div>
         <div v-for="(line, index) in getOutputLines(example)" :key="`output_${index}_line`">
           {{ line }}
           <br />

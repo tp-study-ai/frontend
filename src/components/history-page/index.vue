349d23a0bf0ff37baf97bb9ed3a1c84e780b350e
Kirill Katashinsky <kir20-01@ya.ru>
2023-04-22 12:57:30 +0300

    Added history page

--- src/components/history-page/index.vue
+++ src/components/history-page/index.vue
@@ -1,24 +1,74 @@
 <template>
 <v-container class="px-0">
-  <development-form :value="developmentDialogShown" />
+  <div class="text-h4 mb-2">История</div>
+
+  <v-data-table
+    :headers="tableHeaders"
+    :items="tasks"
+    :items-per-page="15"
+    :loading="loading"
+    hide-default-footer
+    no-data-text="Задачи не найдены"
+    loading-text="Идет загрузка задач..."
+  >
+    <template #item="{ item, isMobile }">
+      <v-card v-if="isMobile" class="mb-4">
+        <v-card-title>{{ item.name }}</v-card-title>
+        <v-card-text>
+          <div>Теги: {{ getTagsByTask(item) }}</div>
+          <div>Рейтинг: {{ item.cf_rating }}</div>
+        </v-card-text>
+
+        <v-card-actions>
+          <v-btn :to="getTaskPath(item)" text color="primary">Перейти</v-btn>
+        </v-card-actions>
+      </v-card>
+
+      <tr v-else>
+        <td>
+          <router-link :to="getTaskPath(item)">{{ item.name }}</router-link>
+        </td>
+        <td>{{ getTagsByTask(item) }}</td>
+        <td>
+          <v-chip :color="getRatingColor(item)" small>
+            {{ getRatingText(item) + (item.cf_rating ? ` (${item.cf_rating})` : '') }}
+          </v-chip>
+        </td>
+      </tr>
+    </template>
+  </v-data-table>
 </v-container>
 </template>
 
 <script>
-import DevelopmentForm from '@/dialogs/development-form';
-
 export default {
   name: 'HistoryPage',
   props: {
     isAuthorized: Boolean
   },
-  components: {
-    DevelopmentForm
-  },
   data() {
     return {
-      developmentDialogShown: true
+      loading: true,
+      tasks: []
     };
+  },
+  computed: {
+    tableHeaders() {
+      return [
+        {
+          text: 'Название',
+          sortable: false
+        },
+        {
+          text: 'Теги',
+          sortable: false
+        },
+        {
+          text: 'Рейтинг',
+          sortable: false
+        }
+      ];
+    }
   },
   watch: {
     isAuthorized(value) {
@@ -33,6 +83,81 @@ export default {
         { text: 'Для перехода на эту страницу необходимо авторизоваться', color: 'error' }
       );
       this.$emit('show:login-form');
+    }
+  },
+  created() {
+    this.$http
+      .get('/get_done_task')
+      .then(({ data: { tasks } }) => {
+        this.tasks = tasks ? tasks : [];
+      })
+      .finally(() => {
+        this.loading = false;
+      });
+  },
+  methods: {
+    getTaskPath(task) {
+      return `/task/${task.id}`;
+    },
+    getTagsByTask(task) {
+      if (!task.cf_tags_RU) {
+        return '';
+      }
+
+      return task.cf_tags_RU.join(', ');
+    },
+    getRatingColor(task) {
+      const { cf_rating } = task;
+      if (cf_rating <= 1200) {
+        return 'purple lighten-2';
+      }
+      if (cf_rating <= 1600) {
+        return 'indigo lighten-2';
+      }
+      if (cf_rating <= 1900) {
+        return 'blue lighten-1';
+      }
+      if (cf_rating <= 2200) {
+        return 'green';
+      }
+      if (cf_rating <= 2500) {
+        return 'yellow';
+      }
+      if (cf_rating <= 2900) {
+        return 'orange';
+      }
+      if (cf_rating <= 3500) {
+        return 'red lighten-2';
+      }
+      return '';
+    },
+    getRatingText(task) {
+      const { cf_rating } = task;
+      if (!cf_rating) {
+        return 'рейтинг неизвестен';
+      }
+      if (cf_rating <= 1200) {
+        return 'новичок';
+      }
+      if (cf_rating <= 1600) {
+        return 'ученик';
+      }
+      if (cf_rating <= 1900) {
+        return 'эксперт';
+      }
+      if (cf_rating <= 2200) {
+        return 'элита';
+      }
+      if (cf_rating <= 2500) {
+        return 'мастер';
+      }
+      if (cf_rating <= 2900) {
+        return 'гроссмейстер';
+      }
+      if (cf_rating <= 3500) {
+        return 'глобальная элита';
+      }
+      return '';
     }
   }
 }

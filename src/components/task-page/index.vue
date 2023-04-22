afecf32911c192e6b844284177fce84f43c54eb5
Kirill Katashinsky <kir20-01@ya.ru>
2023-04-22 15:15:25 +0300

    Changed task page

--- src/components/task-page/index.vue
+++ src/components/task-page/index.vue
@@ -6,7 +6,7 @@
 
   <div v-else>
     <v-row v-touch="swipeOptions">
-      <v-col cols="12" md="6">
+      <v-col cols="12" sm="6">
         <div v-if="$vuetify.breakpoint.xsOnly">
           <v-card v-if="swipeDirection === 'right'">
             <v-card-title>{{ task.title }}</v-card-title>
@@ -121,7 +121,7 @@
         </v-card>
       </v-col>
 
-      <v-col v-if="!$vuetify.breakpoint.xsOnly" cols="12" md="6">
+      <v-col v-if="!$vuetify.breakpoint.xsOnly" cols="12" sm="6">
         <v-card class="d-flex right-card flex-column justify-space-between">
           <code-editor
             v-model="code"

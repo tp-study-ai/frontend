25394396dd4e15b1ddf87bb7c4dea7a17167cce0
Kirill Katashinsky <kir20-01@ya.ru>
2023-05-09 19:59:21 +0300

    Changed config

--- vue.config.js
+++ vue.config.js
@@ -15,6 +15,23 @@ module.exports = defineConfig({
     },
 
     manifestOptions: {
+      icons: [
+        {
+          src: 'favicon.png',
+          sizes: '16x16 24x24 32x32 64x64',
+          type: 'image/png'
+        },
+        {
+          src: 'img/icons/android-chrome-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: 'img/icons/android-chrome-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ]
       display: 'standalone'
     }
   }

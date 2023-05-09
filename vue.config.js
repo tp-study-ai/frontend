a9cd4284f82bed104f1c30c962286be01a04d4de
Kirill Katashinsky <kir20-01@ya.ru>
2023-05-09 19:38:14 +0300

    Changed config

--- vue.config.js
+++ vue.config.js
@@ -6,8 +6,22 @@ module.exports = defineConfig({
   lintOnSave: process.env.NODE_ENV !== 'production',
   pwa: {
     name: 'Study AI',
+    iconPaths: {
+      favicon32: 'favicon.png',
+      favicon16: 'favicon.png',
+      appleTouchIcon: 'favicon.png',
+      maskIcon: null,
+      msTileImage: null
+    },
 
     manifestOptions: {
+      icons: [
+        {
+          src: 'favicon.png',
+          sizes: '16x16 24x24 32x32 64x64',
+          type: 'image/png'
+        }
+      ],
       display: 'standalone'
     }
   }
